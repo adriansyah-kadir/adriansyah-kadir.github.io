9a62689aa766a235b928c5c417792ea2c7408075
adriansyah-kadir <adriansyahkadir.work@gmail.com>
2026-01-30 20:13:01 +0800

    top navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
-	import "$lib/components/css/tailwind.css"
+	import { page } from "$app/state";
+	import "$lib/assets/css/tailwind.css";
+	import IconGithub from "$lib/components/ui/icons/icon-github.svelte";
+	import IconMail from "$lib/components/ui/icons/icon-mail.svelte";
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
-
+<header
+	class="h-14 glass backdrop-blur-xs shadow-none border-b border-base-300 sticky top-0"
+>
+	<div class="max-w-5xl m-auto flex items-center h-full px-4 justify-between">
+		<a href={page.url.origin}>Adriansyah</a>
+		<div class="sm:flex gap-4 hidden">
+			<a href="/blogs">Blogs</a>
+			<a href="/projects">Projects</a>
+		</div>
+		<div class="flex">
+			<a
+				href="mailto:adriansyahkadir.work@gmail.com"
+				class="btn btn-square btn-link"
+			>
+				<IconMail />
+			</a>
+			<a
+				href="https://github.com/adriansyah-kadir"
+				class="btn btn-square btn-link"
+			>
+				<IconGithub />
+			</a>
+		</div>
+	</div>
+</header>
 {@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,3 @@
-<script lang="ts">
-  import IconSpinner from "$lib/components/ui/icons/icon-spinner.svelte";
-  import { AlertDialog } from "bits-ui";
-  import { dialog } from "$lib/components/ui";
-
-  const { content, overlay } = dialog({ animation: true });
-</script>
-
-<AlertDialog.Root>
-  <AlertDialog.Trigger class="btn btn-info">
-    <IconSpinner class="size-4" />
-    Subscribe
-  </AlertDialog.Trigger>
-  <AlertDialog.Portal>
-    <AlertDialog.Overlay
-      class="{overlay()} duration-150"
-    />
-    <AlertDialog.Content class={content()}>
-      <div class="flex flex-col gap-4 pb-6">
-        <AlertDialog.Title class="card-title">
-          Confirm your transaction
-        </AlertDialog.Title>
-        <AlertDialog.Description class="text-base-content/80 text-sm">
-          This action cannot be undone. This will initiate a monthly wire in the
-          amount of $10,000 to Huntabyte. Do you wish to continue?
-        </AlertDialog.Description>
-      </div>
-      <div class="card-actions">
-        <AlertDialog.Cancel class="btn btn-soft">Cancel</AlertDialog.Cancel>
-        <AlertDialog.Action class="btn btn-info">Continue</AlertDialog.Action>
-      </div>
-    </AlertDialog.Content>
-  </AlertDialog.Portal>
-</AlertDialog.Root>
+<div class="max-w-5xl m-auto px-4 h-screen">
+  Homepage
+</div>
