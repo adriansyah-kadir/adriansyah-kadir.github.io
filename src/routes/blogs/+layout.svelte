bb77910a0b2e72e32b223c1f3563b1d0e16fe130
adriansyah-kadir <adriansyahkadir.work@gmail.com>
2026-02-08 21:21:02 +0800

    add blogs layout

---
+++ src/routes/blogs/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import Transition from "$lib/components/transition.svelte";
+  const { children } = $props();
+</script>
+
+<Transition class="w-screen h-screen flex items-center justify-center">
+  {@render children()}
+</Transition>
